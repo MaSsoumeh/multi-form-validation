6365b504962e6fc477b947ff974d402bdd7179db
MaSsoumeh <massoumehzarei@gmail.com>
2021-11-28 00:13:24 +0330

    create Formik wizard

---
+++ src/components/MultiStepForm.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { Formik } from 'formik';
+import FormNavigation from './FormNavigation';
+
+const MultiStepForm = ({ children, initialValues, onSubmit }) => {
+  const [currentStep, setCurrentStep] = useState(0);
+  const steps = React.children.toArray(children);
+  const [snapshot, setSnapshot] = useState(initialValues);
+
+  const step = steps[currentStep];
+  const totalStep = steps.length;
+  const isLastStep = currentStep === totalStep - 1;
+
+  const next = (values) => {
+    setSnapshot(values);
+    setCurrentStep(currentStep + 1);
+  };
+  const previous = (values) => {
+    setSnapshot(values);
+    setCurrentStep(currentStep - 1);
+  };
+
+  const handleSubmit = async (values, actions) => {
+    if (step.props.onSubmit) {
+      await step.props.onSubmit(values);
+    }
+    if (isLastStep) {
+      return onSubmit(values, actions);
+    } else {
+      actions.setTouched({});
+      next(values);
+    }
+  };
+
+  return (
+    <div>
+      {' '}
+      <Formik
+        initialValues={snapshot}
+        onSubmit={handleSubmit}
+        validationSchema={step.props.validationSchema}
+      >
+        {(formik) => (
+          <form onSubmit={formik.handleSubmit}>
+            <FormNavigation
+              isLastStep={isLastStep}
+              hasPrevious={currentStep > 0}
+              onBackClick={previous(formik.values)}
+            />
+          </form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default MultiStepForm;
+
+export const FormStep = ({ stepName = '', children }) => children;
