4e34cf228dc1e1b89e5eabbccfadee35c4cf1603
MaSsoumeh <massoumehzarei@gmail.com>
2021-11-27 05:33:46 +0330

    create formik to manage form state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,73 @@
-import logo from './logo.svg';
+import { Formik } from 'formik';
+import { TextField, Button } from '@mui/material';
+import * as yup from 'yup';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <header className='App-header'>
+        <Formik
+          initialValues={{
+            accidentLocation: '',
+            email: '',
+          }}
+          onSubmit={(values) =>
+            fetch(
+              'https://insurance-3d4b9-default-rtdb.firebaseio.com/form.json',
+              {
+                method: 'POST',
+                body: JSON.stringify(values),
+                headers: {
+                  'content-type': 'application/json',
+                },
+              }
+            )
+          }
+          // validationSchema={validationSchema}
         >
-          Learn React
-        </a>
+          {(formik) => (
+            <form onSubmit={formik.handleSubmit}>
+              <TextField
+                fullWidth
+                style={{ textAlign: 'right' }}
+                id='accidentLocation'
+                label='محل وقوع حادثه*'
+                name='accidentLocation'
+                variant='standard'
+                value={formik.values.accidentLocation}
+                onChange={formik.handleChange}
+                error={
+                  formik.touched.accidentLocation &&
+                  Boolean(formik.errors.accidentLocation)
+                }
+                helperText={
+                  formik.touched.accidentLocation &&
+                  formik.errors.accidentLocation
+                }
+              />
+              <TextField
+                fullWidth
+                id='email'
+                label='ایمیل*'
+                name='email'
+                variant='standard'
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                error={formik.touched.email && Boolean(formik.errors.email)}
+                helperText={formik.touched.email && formik.errors.email}
+              />
+              <Button
+                style={{ marginTop: '20px' }}
+                fullWidth
+                type='submit'
+                variant='contained'
+              >
+                تایید
+              </Button>
+            </form>
+          )}
+        </Formik>
       </header>
     </div>
   );
