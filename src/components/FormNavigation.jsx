0c9fe8e3e0f9d2e517254e5be17a0fa724758459
MaSsoumeh <massoumehzarei@gmail.com>
2021-11-28 00:13:58 +0330

    Create form buttons

---
+++ src/components/FormNavigation.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+const FormNavigation = (props) => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        marginTop: 50,
+        justifyContent: 'space-between',
+      }}
+    >
+      {props.hasPrevious && (
+        <Button variant='text' type='button' onClick={props.onBackClick}>
+          بازگشت به صفحه قبل
+        </Button>
+      )}
+      <Button variant='contained' type={props.isLastStep ? 'submit' : 'button'}>
+        {props.isLastStep ? 'ذخیره' : 'تایید و ادامه'}
+      </Button>
+    </div>
+  );
+};
+
+export default FormNavigation;
