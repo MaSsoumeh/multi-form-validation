4c04d6cff325dad29ccc1bf8597974db4b965f2c
MaSsoumeh <massoumehzarei@gmail.com>
2021-11-28 20:48:32 +0330

    Create Buttons

--- src/components/FormNavigation.jsx
+++ src/components/FormNavigation.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import Button from '@mui/material/Button';
+import { makeStyles } from '@mui/styles';
+import { createTheme } from '@mui/material/styles';
+import { green, blue } from '@mui/material/colors';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: blue[500],
+    },
+    secondary: {
+      main: green[500],
+    },
+  },
+});
+const useStyles = makeStyles(() => ({
+  buttons: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    marginTop: theme.spacing(4),
+    marginLeft: theme.spacing(2),
+  },
+}));
 const FormNavigation = (props) => {
+  const classes = useStyles();
   return (
-    <div
-      style={{
-        display: 'flex',
-        marginTop: 50,
-        justifyContent: 'space-between',
-      }}
-    >
+    <div className={classes.buttons}>
       {props.hasPrevious && (
         <Button variant='text' type='button' onClick={props.onBackClick}>
           بازگشت به صفحه قبل
